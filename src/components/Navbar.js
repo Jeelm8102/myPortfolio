ce4d124e0afe36e3dd3a20ead096f63e357274da
Jeel <jeelmaheshwari812@gmail.com>
2023-08-05 08:39:08 +0530

    created various routes and components.

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import "./NavbarStyle.css";
+
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+        <Link></Link>
+    </div>
+  )
+};
+
+export default Navbar;
