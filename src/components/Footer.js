17b8de9cb4e6e4e6e6be7422c093677c1c01b5fe
Jeel <jeelmaheshwari812@gmail.com>
2023-08-07 20:28:15 +0530

    updated footer and components of the page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "./FooterStyles.css";
 
 import React from 'react'
 
-import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa";
+import { FaFacebook, FaHome, FaInstagram, FaMailBulk, FaPhone, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -13,17 +13,29 @@ const Footer = () => {
                     <FaHome size={20} style={{ color: "#fff", marginRight: "2rem"}}/>
                     <div>
                         <p>123 Housing Society.</p>
-                        <p>Vadodara</p>
+                        <p>Vadodara.</p>
                     </div>
                 </div>
                 <div className="phone">
                     <h4><FaPhone size={20} style={{ color: "#fff", marginRight: "2rem"}}/>
-                    +91-91042-76641</h4>
+                    +91-91042-76641
+                    </h4>
                 </div>
                 <div className="email">
                     <h4><FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem"}}/>
-                    er.jeelmaheshwari@gmail.com</h4>
+                    er.jeelmaheshwari@gmail.com
+                    </h4>
                 </div>
+            </div>
+
+            <div className="right">
+                <h4>About the company</h4>
+                <p>This is Jeel Maheshwari. A creative and enthusiastic web designer.</p>
+                <div className="social">
+                    <FaFacebook size={30} style={{ color: "#fff", marginRight: "1rem"}}/>
+                    <FaTwitter size={30} style={{ color: "#fff", marginRight: "1rem"}}/>
+                    <FaInstagram size={30} style={{ color: "#fff", marginRight: "1rem"}}/>
+                </div>                
             </div>
         </div>
     </div>
