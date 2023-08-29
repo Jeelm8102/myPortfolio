f582fec603ea920b803e7da8e811b645da61e1b1
Jeel <jeelmaheshwari812@gmail.com>
2023-08-29 10:26:00 +0530

    added project card description

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,14 +1,13 @@
 import "./WorkCardStyles.css"
 
-import React from 'react'
-import pro1 from "../assets/project1.png"
+import React from 'react';
 import { NavLink } from "react-router-dom"
 
 const WorkCard = (props) => {
   return (
     <div className="project-card">
         <img src={props.imgsrc} alt="img" />
-        <h2 className="project-title">Project Title</h2>
+        <h2 className="project-title">{props.title}</h2>
         <div className="pro-details">
             <p>{props.text}</p>
             <div className="pro-btns">
