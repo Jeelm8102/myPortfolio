b365a0d9ec5a2b031fb0619c25b583d342ca78de
Jeel <jeelmaheshwari812@gmail.com>
2023-09-04 10:06:06 +0530

    techstack is commented to add it later if required.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,14 +3,14 @@ import Navbar from '../components/Navbar'
 import HeroImg from '../components/HeroImg'
 import Work from '../components/Work'
 import Footer from '../components/Footer'
-import TechStack from '../components/TechStack'
+// import TechStack from '../components/TechStack'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <HeroImg/>
-      <TechStack/>
+      {/* <TechStack/> : tech stack can be added to provide information about you technical skills*/}
       <Work/>
       <Footer/>
     </div>
